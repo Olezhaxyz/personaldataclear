e16c501ee2ebd9b3f216e8c65dc7e2238e3a8f3a
Olezhaxyz <trup.maks@gmail.com>
2023-11-13 17:16:40 +0300

    add on db

--- pyflet/counter.py
+++ pyflet/counter.py
@@ -1,33 +1,23 @@
-import flet
-from flet import ElevatedButton, Page, Text, TextField, icons
+import flet as ft
+import psycopg2
 
-
-def main(page: Page):
-    def button_clicked(e):
-        t.value = (
-            f"Textboxes values are:  "
-            f"'{tb1.value}', "
-            f"'{tb2.value}', "
-            f"'{tb3.value}', "
-            f"'{tb4.value}', "
-            f"'{tb5.value}'."
+class Database:
+    def insert_record(self, last_name, first_name, middle_name, birth_year, phone_number):
+        conn = psycopg2.connect(
+            dbname="personal_db",
+            user="postgres",
+            password="postgres",
+            host="127.0.0.1",
+            port="5432"
         )
-        page.update()
-
-    t = Text()
-    tb1 = TextField(label="Name")
-    tb2 = TextField(label="Surname")
-    tb3 = TextField(label="Lastname")
-    tb4 = TextField(label="Bithday")
-    tb5 = TextField(label="Phone")
-    b = ElevatedButton(text="Submit", on_click=button_clicked)
-    page.add(tb1, tb2, tb3, tb4, tb5, b, t)
-
-
-flet.app(target=main)
-
-
-class Database():
+        cur = conn.cursor()
+        cur.execute('''
+            INSERT INTO people (last_name, first_name, middle_name, birth_year, phone_number)
+            VALUES (%s, %s, %s, %s, %s)
+        ''', (last_name, first_name, middle_name, birth_year, phone_number))
+        conn.commit()
+        conn.close()
+    
     def create_table(self):
         conn = psycopg2.connect(
             dbname="personal_db",
@@ -43,30 +33,48 @@ class Database():
                 last_name VARCHAR(50),
                 first_name VARCHAR(50),
                 middle_name VARCHAR(50),
-                birth_year INTEGER,
+                birth_year VARCHAR(8),
                 phone_number VARCHAR(20)
             )
         ''')
         conn.commit()
         conn.close()
-    def insert_record(self):
-        last_name = self.ids.last_name.text
-        first_name = self.ids.first_name.text
-        middle_name = self.ids.middle_name.text
-        birth_year = int(self.ids.birth_year.text)
-        phone_number = self.ids.phone_number.text
 
-        conn = psycopg2.connect(
-            dbname="personal_db",
-            user="postgres",
-            password="postgres",
-            host="127.0.0.1",
-            port="5432"
-            )
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO people (last_name, first_name, middle_name, birth_year, phone_number)
-            VALUES (%s, %s, %s, %s, %s)
-        ''', (last_name, first_name, middle_name, birth_year, phone_number))
-        conn.commit()
-        conn.close()
+def main(page: ft.Page):
+    
+    database = Database()
+    def button_clicked(e):
+        # Получение значений из текстовых полей
+        name = tb1.value
+        surname = tb2.value
+        lastname = tb3.value
+        birthday = tb4.value
+        phone = tb5.value
+
+        database.create_table()
+        # Запись данных в базу данных
+        database.insert_record(last_name=name, first_name=surname, middle_name=lastname, birth_year=birthday, phone_number=phone)
+
+        # Обновление текстового поля
+        t.value = (
+            f"Textboxes values are:  "
+            f"'{tb1.value}', "
+            f"'{tb2.value}', "
+            f"'{tb3.value}', "
+            f"'{tb4.value}', "
+            f"'{tb5.value}'."
+        )
+        page.update()
+
+    t = ft.Text()
+    tb1 = ft.TextField(label="Name")
+    tb2 = ft.TextField(label="Surname")
+    tb3 = ft.TextField(label="Lastname")
+    tb4 = ft.TextField(label="Birthday")
+    tb5 = ft.TextField(label="Phone")
+    b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
+    
+    page.add(tb1, tb2, tb3, tb4, tb5, b, t)
+
+# Запуск приложения
+ft.app(target=main)
